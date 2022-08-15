c22052aee5a74ef3b7da4ed79a9ef30489154fe0
CasanovaRamiro <Casanovaramiroj@gmail.com>
2022-08-15 12:26:10 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const server = require("./src/app.js");
 
-const PORT = process.env.PORT || 3001 
+const PORT = process.env.PORT || 5000 
 
   server.listen(PORT , () => {
     console.log(`%s listening at ${PORT}`); 
